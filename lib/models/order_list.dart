0c233830df6d7e23703f677e6f3c1eeccee925d6
Helvio Siqueira <67134795+HelvioSiqueira@users.noreply.github.com>
2023-08-30 10:46:06 -0300

    feat: send orders to firebase

--- lib/models/order_list.dart
+++ lib/models/order_list.dart
@@ -1,9 +1,12 @@
+import 'dart:convert';
 import 'dart:math';
 
 import 'package:flutter/material.dart';
+import 'package:shop/utils/constants.dart';
 
 import 'cart.dart';
 import 'order.dart';
+import 'package:http/http.dart' as http;
 
 class OrderList with ChangeNotifier {
   final List<Order> _items = [];
@@ -16,13 +19,33 @@ class OrderList with ChangeNotifier {
     return _items.length;
   }
 
-  void addOrder(Cart cart) {
+  Future<void> addOrder(Cart cart) async {
+    final date = DateTime.now();
+
+    final response =
+        await http.post(Uri.parse("${Constants.ORDER_BASE_URL}.json"),
+            body: jsonEncode({
+              "total": cart.totalAmount,
+              "date": date.toIso8601String(),
+              "products": cart.items.values
+                  .map((cartItem) => {
+                        "id": cartItem.id,
+                        "productId": cartItem.productId,
+                        "name": cartItem.name,
+                        "quantyty": cartItem.quantity,
+                        "price": cartItem.price,
+                      })
+                  .toList()
+            }));
+
+    var id = jsonDecode(response.body)["name"];
+
     _items.insert(
         0,
         Order(
-            id: Random().nextDouble().toString(),
+            id: id,
             total: cart.totalAmount,
-            date: DateTime.now(),
+            date: date,
             products: cart.items.values.toList()));
 
     notifyListeners();
